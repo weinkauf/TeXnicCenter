ee0dc7cdffa40bea989f34654046abf4c9bdc609
Sven Wiegand <2285034+svenwiegand@users.noreply.github.com>
2002-06-19 16:11:14 +0000

    - Made some german translation of new strings. - Updated version ID. - Added XP-manifest file to make TXC use new controls on WinXP.

--- TeXnicCenter/resource.h
+++ TeXnicCenter/resource.h
@@ -1169,7 +1169,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        264
+#define _APS_NEXT_RESOURCE_VALUE        265
 #define _APS_NEXT_COMMAND_VALUE         38345
 #define _APS_NEXT_CONTROL_VALUE         1300
 #define _APS_NEXT_SYMED_VALUE           115
