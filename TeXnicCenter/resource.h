bf319147143559811189e68a3a7e7e597c009484
Sven Wiegand <2285034+svenwiegand@users.noreply.github.com>
2003-12-16 10:51:20 +0000

    Added drop-down-lists for 'language'- and 'look & feel'-selection on general options tab (Codes will follow soon).

--- TeXnicCenter/resource.h
+++ TeXnicCenter/resource.h
@@ -622,7 +622,9 @@
 #define IDC_STATIC_IMPORT               1331
 #define IDC_BUTTON_SELECT_INVERT        1332
 #define IDC_BUTTON_SELECT_NONE          1333
+#define IDC_GUI_LANGUAGE_COMBO          1333
 #define IDC_BUTTON_SELECT_ALL           1334
+#define IDC_LOOKNFEEL_COMBO             1334
 #define IDD_CUSTOMIZEPAGE_EDITOR        16105
 #define IDD_OUTPUTWIZARD_MIKTEX         16106
 #define IDD_OUTPUTWIZARD_PATH           16107
@@ -1248,9 +1250,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        274
+#define _APS_NEXT_RESOURCE_VALUE        276
 #define _APS_NEXT_COMMAND_VALUE         38467
-#define _APS_NEXT_CONTROL_VALUE         1333
+#define _APS_NEXT_CONTROL_VALUE         1334
 #define _APS_NEXT_SYMED_VALUE           115
 #endif
 #endif
