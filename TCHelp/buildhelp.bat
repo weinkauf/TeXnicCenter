0632083009d5cf76869ee98bfdedfd50bbcda742
Oliver Wieland <Oliver Wieland@localhost>
2005-05-20 18:08:48 +0000

    Provide batch files for additional build process. They may be added to  the custom build step.
    buildhelp.bat: Compiles the help project and moves all help files to the output dirs

---
+++ TCHelp/buildhelp.bat
@@ -0,0 +1,26 @@
+@echo off
+rem Build all help files and moves the created files resp. additional help files
+rem to the appropriate output directories.
+rem $Id$
+rem
+
+rem hhc returns 1, even if it was successful
+@hhc TeXnicCenter.hhp
+@if %errorlevel% GEQ 2 goto error
+
+@cd ..\output
+@echo "Copying created help files..."
+
+@for %%i in (*.chm;*.chw) do copy %%i .\Debug\help
+@for %%i in (*.chm;*.chw) do copy %%i .\Release\help
+@cd ..\texniccenter
+
+@echo "Copying additional help files..."
+@for %%i in (GPL_english.htm GPL_german.htm Tips_english.txt Tips_german.txt UserImages.bmp) do copy %%i ..\output\Debug
+@for %%i in (GPL_english.htm GPL_german.htm Tips_english.txt Tips_german.txt UserImages.bmp) do copy %%i ..\output\Release
+
+cd ..\TCHelp
+goto finish
+:error
+@echo "Build failed!"
+:finish
