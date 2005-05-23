76453cb7e16d2b1258d9823410fe5cbb87bb6f69
Oliver Wieland <Oliver Wieland@localhost>
2005-05-23 20:45:49 +0000

    Removed 'pause' calls

--- TCHelp/buildhelp.bat
+++ TCHelp/buildhelp.bat
@@ -9,21 +9,17 @@ rem hhc returns 1, even if it was successful
 @if %errorlevel% GEQ 2 goto error
 
 @cd ..\output
-pause
 @echo "Creating help directories..."
 if not exist .\Release\help (
 md .\Release\help
 )
-pause
 if not exist .\Debug\help (
 md .\Debug\help
 ) 
-pause
 @echo "Copying created help files..."
 @for %%i in (*.chm;*.chw) do copy %%i .\Debug\help
 @for %%i in (*.chm;*.chw) do copy %%i .\Release\help
 @cd ..\texniccenter
-pause
 @echo "Copying additional help files..."
 @for %%i in (GPL_english.htm GPL_german.htm Tips_english.txt Tips_german.txt UserImages.bmp) do copy %%i ..\output\Debug
 @for %%i in (GPL_english.htm GPL_german.htm Tips_english.txt Tips_german.txt UserImages.bmp) do copy %%i ..\output\Release
