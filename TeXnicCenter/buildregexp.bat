546211b2e43f8fdaa8edf6ba1d53448e5d85c173
Oliver Wieland <Oliver Wieland@localhost>
2005-06-04 11:07:47 +0000

    Bugfix: Create output/Debug resp. output/Release before copying files (otherwise copy will create files and build of txc will fail)

--- TeXnicCenter/buildregexp.bat
+++ TeXnicCenter/buildregexp.bat
@@ -5,6 +5,14 @@ rem
 
 cd ..\regexpp\lib
 call vc6.bat
+
+if not exist ..\..\output\Release (
+md ..\..\output\Release
+)
+if not exist ..\..\output\Debug (
+md ..\..\output\Debug
+) 
+
 copy mre200dl.dll ..\..\output\Debug
 copy mre200dl.ilk ..\..\output\Debug
 copy mre200dl.pdb ..\..\output\Debug
