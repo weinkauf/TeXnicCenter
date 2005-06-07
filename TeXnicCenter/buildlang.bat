772877047b2f72627d32bd72b276d61ee94d5a2f
Oliver Wieland <Oliver Wieland@localhost>
2005-06-07 22:59:59 +0000

    - Added missing file operations - Added packages.xml to file list

--- TeXnicCenter/buildlang.bat
+++ TeXnicCenter/buildlang.bat
@@ -14,4 +14,8 @@ md ..\output\debug\language
 @cd ..\myspell
 @for %%i in (*.aff;*.dic) do copy %%i ..\output\release\language
 @for %%i in (*.aff;*.dic) do copy %%i ..\output\debug\language
-@cd ..
+@cd ..\TeXnicCenter
+
+@for %%i in (GPL_english.htm GPL_german.htm Tips_english.txt Tips_german.txt UserImages.bmp Packages.xml) do copy %%i ..\output\release
+@for %%i in (GPL_english.htm GPL_german.htm Tips_english.txt Tips_german.txt UserImages.bmp Packages.xml) do copy %%i ..\output\debug
+
