90bdc7fe408763870bf35cdee2771e36789aff77
Oliver Wieland <Oliver Wieland@localhost>
2005-06-16 20:55:23 +0000

    - Added copyright notice - Derive from CStructureItem - Added BibTeX support / navigator

--- TeXnicCenter/BiBTeXFile.h
+++ TeXnicCenter/BiBTeXFile.h
@@ -1,6 +1,36 @@
-// BiBTeXFile.h: interface for the CBiBTeXFile class.
-//
-//////////////////////////////////////////////////////////////////////
+/********************************************************************
+*
+* This file is part of the TeXnicCenter-system
+*
+* Copyright (C) 1999-2000 Sven Wiegand
+* Copyright (C) 2000-$CurrentYear$ ToolsCenter
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 2 of
+* the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+* If you have further questions or if you want to support
+* further TeXnicCenter development, visit the TeXnicCenter-homepage
+*
+*    http://www.ToolsCenter.org
+*
+*********************************************************************/
+
+/********************************************************************
+*
+* $Id$
+*
+********************************************************************/
 
 #if !defined(AFX_BIBTEXFILE_H__8631BC01_4CE2_4706_995E_5230B32EF3FA__INCLUDED_)
 #define AFX_BIBTEXFILE_H__8631BC01_4CE2_4706_995E_5230B32EF3FA__INCLUDED_
@@ -40,11 +70,13 @@ public:
 	/** Removes all BibTeX entries */
 	void DropAllEntries();
 private:
+	/* Called, if an item is complete */
+	void FinalizeItem();
 	/* Parses a field entry 'name = value' */
 	BOOL ParseField(const TCHAR *field, CString &name, CString &val);
 	void HandleParseError(const TCHAR *buf, int line, int col);
-	CBiBTeXEntry::BibType ProcessEntryType(const TCHAR *buf, int len);
-	void ProcessArgument(const TCHAR *buf, int len, CBiBTeXEntry::BibType);
+	CBiBTeXEntry::BibType ProcessEntryType(const TCHAR *buf, int len, int line);
+	void ProcessArgument(const TCHAR *buf, int len, CBiBTeXEntry::BibType, int line);
 	BOOL ParseFile(const TCHAR* buf);
 	
 
