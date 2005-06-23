cfc8442b2a37b8da258f04ca8bccbe4d25cf732c
Oliver Wieland <Oliver Wieland@localhost>
2005-06-23 21:20:05 +0000

    - Not all batch files were invoked => added "call" to each entry - Added buildpack.bat to copy package files

--- TeXnicCenter/buildxml.bat
+++ TeXnicCenter/buildxml.bat
@@ -3,6 +3,7 @@ rem Copy all xml schema files to the output directories
 rem $Id$
 rem
 @cd xmlschema
+@echo Copying xml schema files...
 @for %%i in (*.xsd) do copy %%i ..\..\output\release
 @for %%i in (*.xsd) do copy %%i ..\..\output\debug
 @cd ..
