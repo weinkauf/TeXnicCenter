8facd113f7c9aed18be58f28e48a88f17941063e
Tino Weinkauf <24574426+weinkauf@users.noreply.github.com>
2006-06-05 12:51:39 +0000

    Can be initialized using an init path.

--- MFCExt/Source/FolderSelect.h
+++ MFCExt/Source/FolderSelect.h
@@ -45,13 +45,16 @@ class AFX_EXT_CLASS CFolderSelect
 {
 // construction/destruction
 public:
-	/**
-	Constructs the CFolderSelect-object. Call DoModal() to display the
-	dialog.
+	/** Constructs the CFolderSelect-object. Call DoModal() to display the dialog.
 
-	See description of BROWSEINFO in the MSDN for parameters.
+		See description of BROWSEINFO in the MSDN for parameters.
 	*/
 	CFolderSelect( LPCTSTR lpszTitle, UINT unFlags = 0, CWnd *pwndParent = NULL, BFFCALLBACK lpfn = NULL, LPARAM lParam = 0 );
+	
+	/** Constructs the CFolderSelect-object. Calling DoModal() will display the dialog with the given initial directory.
+	*/
+	CFolderSelect( LPCTSTR lpszTitle, LPCTSTR lpszInitPath, UINT unFlags = 0, CWnd *pwndParent = NULL );
+
 	virtual ~CFolderSelect();
 
 // operations
@@ -83,9 +86,13 @@ public:
 	*/
 	int GetImage();
 
+private:
+	///Inits the internal structures.
+	void Init(LPCTSTR lpszTitle, UINT unFlags = 0, CWnd* pwndParent = NULL, BFFCALLBACK lpfn = NULL, LPARAM lParam = 0);
+
 // attributes
 protected:
-	/** Titel, der angezeigt werden soll. */
+	/** Title to be shown at the top of the dialog. */
 	CString	m_strTitle;
 
 	/** Structure for SHBrowseForFolder filled by the constructor. */
@@ -99,6 +106,9 @@ protected:
 
 	/** Image associated with the selected folder. */
 	int m_nImage;
+
+	///Holds the initial folder.
+	CString m_strInitFolder;
 };
 
 
@@ -122,6 +132,18 @@ int CFolderSelect::GetImage()
 {
 	return m_nImage;
 }
+
+
+/**
+This is the callback function for the FolderSelect-Dialog.
+Using this we are able to send a message to the dialog,
+which selects a new folder. The pointer to the initial folder
+needs to be given in the BROWSEINFO::lParam parameter.
+
+@author Tino Weinkauf
+*/
+int CALLBACK FolderSelectDlgCallback(HWND hwnd, UINT uMsg, LPARAM lParam, LPARAM lpData);
+
 
 
 #endif // !defined(AFX_FOLDERSELECT_H__8D566541_1865_11D4_A221_006097239934__INCLUDED_)
