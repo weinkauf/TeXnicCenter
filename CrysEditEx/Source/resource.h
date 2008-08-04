fe56a49da09c7e10d7f8274c53750d7ef27c1214
Tino Weinkauf <24574426+weinkauf@users.noreply.github.com>
2008-08-04 17:21:45 +0000

    New Buildsystem works without batch files and uses the output directory for all intermediate and final files.
    Templates are still to come

--- CrysEditEx/Source/resource.h
+++ CrysEditEx/Source/resource.h
@@ -7,10 +7,10 @@
 #define ID_EDIT_QUERY_COMPLETION        58112
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40004
 #define _APS_NEXT_CONTROL_VALUE         1019
 #define _APS_NEXT_SYMED_VALUE           101
