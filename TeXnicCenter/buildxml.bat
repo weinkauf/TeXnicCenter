122cdfa5f89af8997fb04d2e755343933bffab6a
Tino Weinkauf <Tino Weinkauf@localhost>
2008-08-04 17:21:45 +0000

    New Buildsystem works without batch files and uses the output directory for all intermediate and final files.
    Templates are still to come

--- TeXnicCenter/buildxml.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Copy all xml schema files to the output directories
-rem $Id$
-rem
-@cd xmlschema
-@echo Copying xml schema files...
-@for %%i in (*.xsd) do copy %%i ..\..\output\release
-@for %%i in (*.xsd) do copy %%i ..\..\output\debug
-@cd ..
