a3bf6dab184dc032f3bc6f4e911213938815819e
Tino Weinkauf <24574426+weinkauf@users.noreply.github.com>
2010-04-09 04:55:35 +0000

    Navigator Views with the following changed properties: No automatic horizontal scrolling Arrows are not faded in/out Double-click does not collapse/expand sections anymore, but still folders.

--- TeXnicCenter/navigatorview.h
+++ TeXnicCenter/navigatorview.h
@@ -150,7 +150,7 @@ protected:
 	afx_msg void OnSysColorChange();
 	afx_msg void OnSelChanged(NMHDR* pNMHDR, LRESULT* pResult);
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
-	afx_msg void OnNMDblclk(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnLButtonDblClk(UINT nFlags, CPoint point);
 	afx_msg void OnDestroy();
 	afx_msg void OnContextMenu(CWnd* /*pWnd*/, CPoint /*point*/);
 	virtual BOOL PreTranslateMessage(MSG* pMsg);
