0daf653ce9f00212fa8dd6dccc4729678d1ba7ff
Tino Weinkauf <24574426+weinkauf@users.noreply.github.com>
2012-01-31 12:26:24 +0100

    Small changes to the source code layout.

--- TeXnicCenter/LatexOutputFilter.cpp
+++ TeXnicCenter/LatexOutputFilter.cpp
@@ -294,18 +294,18 @@ DWORD CLaTeXOutputFilter::ParseLine(const CString& strLine, DWORD dwCookie)
 
 	/* ABOUT THE ORDER OF SEARCHING THE OUTPUT
 
-	                Every TeX-User needs to correct an error, if he wants to have a valid document.
-	                Therefore, errors are quite rare.
-	                For some warnings, but not all, there is also a need to correct them - to get a better document.
-	                Therefore, warnings may occur more often than errors.
-	                Most users are not going to correct a bad box. Sometimes it is not possible at all (at least
-	                for the average TeX-user).
-	                Therefore, bad boxes are the most common type of catched entities.
+		Every TeX-User needs to correct an error, if he wants to have a valid document.
+		Therefore, errors are quite rare.
+		For some warnings, but not all, there is also a need to correct them - to get a better document.
+		Therefore, warnings may occur more often than errors.
+		Most users are not going to correct a bad box. Sometimes it is not possible at all (at least
+		for the average TeX-user).
+		Therefore, bad boxes are the most common type of catched entities.
 
-	                To speed up parsing the output, it is preferable to look for the common stuff first.
-	                ==> 1. bad boxes; 2. warnings; 3. errors; 4. srcline-numbers; 5. other stuff
+		To speed up parsing the output, it is preferable to look for the common stuff first.
+		==> 1. bad boxes; 2. warnings; 3. errors; 4. srcline-numbers; 5. other stuff
 
-	 */
+	*/
 	LPCTSTR const text = strLine;
 
 	if (regex_search(text,results,badBox1))
