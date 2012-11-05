5082406cbca63be359c9b10a27af54174a23deb4
Sergiu Dotenco <sergiu.dotenco@gmail.com>
2012-11-05 11:40:13 +0100

    cmake: added partial nmake support

--- modules/mfcext/CollectionException.cpp
+++ modules/mfcext/CollectionException.cpp
@@ -4,9 +4,9 @@
 *
 * Copyright (C) 1999-2000 Sven Wiegand
 * Copyright (C) 2000-$CurrentYear$ ToolsCenter
-* 
+*
 * This library is free software; you can redistribute it and/or
-* modify, but leave the headers intact and do not remove any 
+* modify, but leave the headers intact and do not remove any
 * copyrights from the source.
 *
 * This library does not only contain file from us, but also from
