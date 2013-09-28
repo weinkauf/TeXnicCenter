4ff8edd12d3365e73680f6b3e7493ec83cd76a82
Tino Weinkauf <24574426+weinkauf@users.noreply.github.com>
2013-09-28 22:58:24 +0200

    Version 2.02

--- TeXnicCenter/Version.h
+++ TeXnicCenter/Version.h
@@ -4,7 +4,7 @@
 #define TXC_TO_STRING(x)	 TXC_STRINGIFY(x)
 
 #define TXC_MAJOR_VERSION    2
-#define TXC_MINOR_VERSION    01
+#define TXC_MINOR_VERSION    02
 #define TXC_REVISION         0
 #define TXC_BUILD_NUMBER     0
 
